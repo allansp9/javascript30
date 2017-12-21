fc298e036c4dc2698060825ccf820387ab75dea6
Allan Pereira <allansp9@gmail.com>
2017-12-21 20:37:54 -0200

    added key transition javascript

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,9 +1,20 @@
-(function() {
-  window.addEventListener("keyup", e => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if (audio) {
-      audio.currentTime = 0;
-      audio.play();
-    }
-  });
-})();
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (audio) {
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  key.classList.toggle("pressed");
+}
+
+function removeTransition(e) {
+  this.classList.remove("pressed");
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
