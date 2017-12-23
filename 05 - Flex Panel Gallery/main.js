c643dbd2b48a6a652088c4e9e582a3cac451c1ee
Allan Pereira <allansp9@gmail.com>
2017-12-23 19:43:07 -0200

    fixed toggleOpen

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -1,8 +1,12 @@
 const panels = document.querySelectorAll(".panel");
 
 function toggleOpen() {
-  panels.forEach(panel => panel.classList.remove("open"));
   this.classList.toggle("open");
+  panels.forEach(panel => {
+    if (panel != this) {
+      panel.classList.remove("open");
+    }
+  });
 }
 
 function toggleActive(e) {
